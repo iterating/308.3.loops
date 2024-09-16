3ef68d8cd72106355bf362d8da279a1f7f06ff2c
Jonathan Young <jy>
2024-09-16 09:40:40 -0700

    parseCSV

--- 308.3.1.practicalloops.js
+++ 308.3.1.practicalloops.js
@@ -58,3 +58,57 @@ function findNextPrime(n) {
     }
 }
 findNextPrime(15);
+
+// Part 3: Feeling Loopy
+
+function parseCSV(input) {
+// Each row will be an array of strings. The table will be an array of rows (2d array)
+    let selectCell = '';
+    let selectRow = [];
+    let csvRows = [];
+
+// Loop through each character and put it into the row array, delimited by ","
+    for (let i = 0; i < input.length; i++) {
+         let selected = input[i];
+         switch (selected){
+             // "," delimits a cell, push cell into row and start a new one
+            case ',':
+                // push character to row array
+                selectRow.push(selectCell);
+                // reset the cell select
+                selectCell = '';
+                // push last cell that isnt delimited
+                if (selectCell.length > 0 ) {
+                    selectRow.push(selectCell);
+                }
+                break;
+            case '\n':
+                // \n delimits a new row
+                if (input[i] == '\n') {
+                // push cell into row array
+                selectRow.push(selectCell);
+                // push rows into array of all rows
+                csvRows.push(selectRow);
+                // reset the cell and row select
+                selectCell = '';
+                selectRow = [];
+                // push last row that isnt delimited
+                if (selectRow.length > 0 ) {
+                    csvRows.push(selectRow);
+                }
+            }
+            break;
+            i++;
+        default: 
+            // add each character to the selected cell 
+            selectCell += selected;
+            break;
+        }
+    }
+// Render the table
+    console.log(csvRows)
+}
+
+// Example CSV string
+input = "Index,Mass (kg),Spring 1 (m),Spring 2 (m)\n1,0.00,0.050,0.050\n2,0.49,0.066,0.066\n3,0.98,0.087,0.080\n4,1.47,0.116,0.108\n5,1.96,0.142,0.138\n6,2.45,0.166,0.158\n7,2.94,0.193,0.174\n8,3.43,0.204,0.192\n9,3.92,0.226,0.205\n10,4.41,0.238,0.232"
+parseCSV(input);
