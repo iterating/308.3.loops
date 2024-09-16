bc46d30f9804bd205a1236af9a79665ef535fc91
Jonathan Young <jy>
2024-09-16 02:54:46 -0700

    error handling

--- 308.3.1.practicalloops.js
+++ 308.3.1.practicalloops.js
@@ -1,5 +1,17 @@
 function fizzBuzz(range, match1, match2){
-    for (i = 1; i <=range; i++) {
+    if (typeof range !== 'number' || isNaN(range) || range <= 0 || !Number.isInteger(range)) {
+        console.error("Select a positive integer for the range");
+        return;
+    }
+    if (typeof match1 !== 'number' || isNaN(match1) || match1 <= 0 || !Number.isInteger(match1)) {
+        console.error("Argument match1 must be a positive integer.");
+        return;
+    }
+    if (typeof match2 !== 'number' || isNaN(match2) || match2 <= 0 || !Number.isInteger(match2)) {
+        console.error("Argument match2 must be a positive integer.");
+        return;
+    }    
+    for (let i = 1; i <=range; i++) {
         if (i % match1 == 0 && i % match2 == 0){
             console.log(`FizzBuzz`)
         } else if (i % match1 == 0){
@@ -11,3 +23,4 @@ function fizzBuzz(range, match1, match2){
     }
 }
 fizzBuzz(100, 3, 5)
+
