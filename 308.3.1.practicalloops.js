12fb9841bc0ebd3c549d4e6169521732ab651860
Jonathan Young <jy>
2024-09-16 03:02:38 -0700

    Error handling DRY

--- 308.3.1.practicalloops.js
+++ 308.3.1.practicalloops.js
@@ -1,16 +1,14 @@
 function fizzBuzz(range, match1, match2){
-    if (typeof range !== 'number' || isNaN(range) || range <= 0 || !Number.isInteger(range)) {
-        console.error("Select a positive integer for the range");
-        return;
+    function validateParams(param){
+        if (typeof param !== 'number' || isNaN(param) || param <= 0 || !Number.isInteger(param)) {
+        throw new Error("Select positive integers for the parameters")
+        }
     }
-    if (typeof match1 !== 'number' || isNaN(match1) || match1 <= 0 || !Number.isInteger(match1)) {
-        console.error("Argument match1 must be a positive integer.");
-        return;
-    }
-    if (typeof match2 !== 'number' || isNaN(match2) || match2 <= 0 || !Number.isInteger(match2)) {
-        console.error("Argument match2 must be a positive integer.");
-        return;
-    }    
+    try {
+        validateParams(range);
+        validateParams(match1);
+        validateParams(match2);
+
     for (let i = 1; i <=range; i++) {
         if (i % match1 == 0 && i % match2 == 0){
             console.log(`FizzBuzz`)
@@ -19,8 +17,12 @@ function fizzBuzz(range, match1, match2){
         } else if (i % match2 == 0){
             console.log(`Buzz`)
         } else {console.log(i); 
+            }
         }
+    } catch (err){
+        console.error(err.message)
     }
-}
+}   
+
 fizzBuzz(100, 3, 5)
 
